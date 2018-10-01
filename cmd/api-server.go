bdd4d7d398a7588d0f66cd4073cc68123985c1cc
zhangmingkai4315 <zhangmingkai19892013@gmail.com>
2018-10-01 22:27:35 +0800

    add struct

---
+++ cmd/api-server.go
@@ -0,0 +1,22 @@
+package cmd
+
+import (
+	"log"
+	"net/http"
+	"os"
+
+	"github.com/zhangmingkai4315/go-storage/heartbeat"
+	"github.com/zhangmingkai4315/go-storage/objects"
+)
+
+func RunAPIServer() {
+
+	go heartbeat.StartHeartBeat()
+
+	// go locate.StartLocate()
+
+	hostAndPort := os.Getenv("STORAGE_PORT")
+	http.HandleFunc("/objects/", objects.Handler)
+	log.Println("storage server listen at port " + hostAndPort)
+	log.Fatal(http.ListenAndServe(hostAndPort, nil))
+}
--- main.go
+++ main.go
@@ -1,22 +1,5 @@
 package main
 
-import (
-	"log"
-	"net/http"
-	"os"
-
-	"github.com/zhangmingkai4315/go-storage/heartbeat"
-	"github.com/zhangmingkai4315/go-storage/objects"
-)
-
 func main() {
 
-	go heartbeat.StartHeartBeat()
-
-	// go locate.StartLocate()
-
-	hostAndPort := os.Getenv("STORAGE_PORT")
-	http.HandleFunc("/objects/", objects.Handler)
-	log.Println("storage server listen at port " + hostAndPort)
-	log.Fatal(http.ListenAndServe(hostAndPort, nil))
 }
